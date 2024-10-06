973769bd29ebcddf638fb29e06d04a0bf018315d
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2024-10-06 06:59:09 +0200

    Update hd.js

--- src/plugin/hd.js
+++ src/plugin/hd.js
@@ -23,7 +23,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
 
     try {
         let proses = await remini(media, "enhance"); // Call remini directly
-        gss.sendMessage(m.from, { image: proses, caption: `> *Hey ${m.pushName} Here Is Your Enhanced Image By 𝙹𝙾𝚎𝚕*` }, { quoted: m });
+        gss.sendMessage(m.from, { image: proses, caption: `> *Hey ${m.pushName} Here Is Your Enhanced Image By Scopion bot*` }, { quoted: m });
       
     } catch (error) {
       console.error('Error processing media:', error);
